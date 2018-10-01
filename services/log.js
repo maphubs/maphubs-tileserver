4799b522cac57ef97428a358d30ffcebdcf76f3e
Kristofor Carle <kris@maphubs.com>
2018-10-01 15:42:21 -0400

    upgrade deps

--- services/log.js
+++ services/log.js
@@ -1,4 +1,27 @@
-/* @flow weak */
-var winston = require('winston');
+// @flow
+var winston = require('winston')
+require('winston-daily-rotate-file')
 
-module.exports = winston;
+var hostname = process.env.HOSTNAME || 'local'
+
+var fileTransport = new (winston.transports.DailyRotateFile)({
+  filename: `logs/maphubs-tiles-${hostname}-%DATE%.log`,
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d'
+})
+
+var logger = winston.createLogger({
+  transports: [
+    fileTransport,
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
+  ]
+})
+
+module.exports = logger
